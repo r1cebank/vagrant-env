0bab329972b1675c1e9fb7b5400b98c7a67ae22b
Siyuan Gao <siyuan.gao@move.com>
2018-02-09 20:12:17 -0800

    finishing up tensotflow vagrant

--- tensorflow/sh/core.sh
+++ tensorflow/sh/core.sh
@@ -6,8 +6,11 @@ apt-get install -y build-essential \
                    git curl vim libcairo2-dev \
                    libav-tools nfs-common portmap apt-transport-https \
                    ca-certificates software-properties-common
+apt-get install -y python3-pip python3-venv
 
 curl https://get.docker.com | sh
 
 # add user to docker group
 usermod -aG docker ubuntu
+
+su -c "source /vagrant/sh/user-config.sh" ubuntu
---
+++ tensorflow/sh/user-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir venv
+cd venv
+python3 -m venv tensorflow
+
+source tensorflow/bin/activate
+
+pip install tensorflow numpy pandas jupyter
